fdf948426c0f2ff35809084b27526d65d8380f24
Daniel Gillespie <gosstapher@gmail.com>
2018-10-28 11:43:17 +0000

    changed render method and subscribed to store for updates

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
-const state = store.getState()
+const render = () => {
+	const state = store.getState()
+	ReactDOM.render(<App {...state} />, document.getElementById('root'));
+}
 
-ReactDOM.render(<App {...state} />, document.getElementById('root'));
+render()
+
+store.subscribe(render)
+
+setTimeout(() => {
+	store.dispatch({type: 'ADVISOR_ADD', payload: {id: 6, name: "Advisor 6", research_area: "Marketing", max_pg: 3, max_ug: 4},})
+}, 1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
